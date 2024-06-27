a7d0e1ef8c96bcfbb1ea967bd4b4a527401a9bd6
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-06-27 04:42:22 +0330

    Create vxlan.sh

---
+++ vxlan.sh
@@ -0,0 +1,48 @@
+install_iproute() {
+sudo apt update
+sudo apt install iproute2 bridge-utils
+}
+
+Iran() {
+sudo ip link add vxlan0 type vxlan id 10 dev eth0 remote $remote_ip dstport 4789
+sudo ip addr add 10.0.0.1/24 dev vxlan0
+sudo ip link set up vxlan0
+}
+
+kharej() {
+sudo ip link add vxlan0 type vxlan id 10 dev eth0 remote $remote_ip dstport 4789
+sudo ip addr add 10.0.0.2/24 dev vxlan0
+sudo ip link set up vxlan0
+}
+remove_full() {
+    
+}
+while true; do
+  echo ""
+  echo "1) Iran"
+  echo "2) kharej"
+  echo "3) Remove Completely"
+  echo "9) Back"
+  read -p "Enter your choice: " choice
+
+  case $choice in
+    1)
+      install_iproute
+      Iran
+      ;;
+    2)
+      install_iproute
+      kharej
+      ;;
+    3)
+      remove_full
+      ;;
+    9)
+      echo "Exiting..."
+      break
+      ;;
+    *)
+      echo "Invalid option. Please try again."
+      ;;
+  esac
+done
